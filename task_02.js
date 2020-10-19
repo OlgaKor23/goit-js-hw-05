892c9d94f79afae4f642d8de0b1d0427b8aef2a6
macbook <jackiepro23@gmail.com>
2020-10-19 19:17:08 +0300

    goit-js-hw-05

---
+++ task_02.js
@@ -0,0 +1,28 @@
+class User {
+	constructor(args) {
+		this.name = args.name;
+		this.age = args.age;
+		this.followers = args.followers;
+	}
+
+	getInfo() {
+		return(`User ${this.name} is ${this.age} years old and has ${this.followers}`);
+	}
+}
+
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
---
+++ task_04.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+	constructor(value)  {
+		this._value = value;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	append(str) {
+		// this._value = this._value += str;
+		this._value = this._value.concat(str);
+	}
+
+	prepend(str) {
+		this._value = str.concat(this._value);
+	}
+
+	pad(str) {
+		this._value = str.concat(this._value,  '=');
+	}
+
+}
+
+console.dir(StringBuilder);
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
